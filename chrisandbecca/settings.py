3c7056a760ea0f601a55d8ef0835fa906511dcfd
chris.charles.jones@gmail.com <Chris Jones>
2019-12-06 21:31:55 -0800

    adding local_settings.py

--- chrisandbecca/settings.py
+++ chrisandbecca/settings.py
@@ -24,12 +24,6 @@ SECRET_KEY = '8=b00$u-l=x#imj@taoxauizc700id$usocvv8pia$9rmadf9x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'chrisandbecca.com',
-    'www.chrisandbecca.com',
-    'localhost'
-]
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -76,12 +70,7 @@ WSGI_APPLICATION = 'chrisandbecca.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -117,15 +106,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 try:
     import chrisandbecca.secrets
+    from local_settings import *
 except ImportError:
     pass
